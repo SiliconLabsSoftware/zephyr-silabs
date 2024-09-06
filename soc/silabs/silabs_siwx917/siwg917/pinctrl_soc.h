d3d717943d9ee3db2c614a71e11e325e8ef15511
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2024-09-06 16:24:25 +0200

    drivers: pinctrl: siwx917: Refactor pinctrl driver
    * Correctly select and enable pads.
    * Add named macros for all valid pin selections.
    * Enable selection of HP peripherals on ULP pins.
    * Enable selection of ULP peripherals on HP pins.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- soc/silabs/silabs_siwx917/siwg917/pinctrl_soc.h
+++ soc/silabs/silabs_siwx917/siwg917/pinctrl_soc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2023 Antmicro
+ * Copyright (c) 2024 Silicon Laboratories, Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,29 +8,30 @@
 #define ZEPHYR_SOC_SILABS_SIWX917_PINCTRL_SOC_H_
 
 #include <zephyr/types.h>
+#include <zephyr/dt-bindings/pinctrl/silabs/siwx917-pinctrl.h>
 
 typedef struct pinctrl_soc_pin_t {
-	uint8_t base;
 	uint8_t port;
 	uint8_t pin;
-	uint8_t mux;
+	uint8_t ulppin;
+	uint8_t mode;
+	uint8_t ulpmode;
 	uint8_t pad;
 } pinctrl_soc_pin_t;
 
-/* FIXME: this layout does not allow to declare multiple pins */
-#define SILABS_SIWX917_DT_PIN(node_id)				\
-	{							\
-		.base = DT_PROP_BY_IDX(node_id, pinmux, 0),	\
-		.port = DT_PROP_BY_IDX(node_id, pinmux, 1),	\
-		.pin  = DT_PROP_BY_IDX(node_id, pinmux, 2),	\
-		.mux  = DT_PROP_BY_IDX(node_id, pinmux, 3),	\
-		.pad  = DT_PROP_BY_IDX(node_id, pinmux, 4),	\
+#define Z_PINCTRL_STATE_PIN_INIT(node, prop, idx)                                                  \
+	{                                                                                          \
+		.port = FIELD_GET(SIWX917_PINCTRL_PORT_MASK, DT_PROP_BY_IDX(node, prop, idx)),     \
+		.pin = FIELD_GET(SIWX917_PINCTRL_PIN_MASK, DT_PROP_BY_IDX(node, prop, idx)),       \
+		.ulppin = FIELD_GET(SIWX917_PINCTRL_ULPPIN_MASK, DT_PROP_BY_IDX(node, prop, idx)), \
+		.mode = FIELD_GET(SIWX917_PINCTRL_MODE_MASK, DT_PROP_BY_IDX(node, prop, idx)),     \
+		.ulpmode =                                                                         \
+			FIELD_GET(SIWX917_PINCTRL_ULPMODE_MASK, DT_PROP_BY_IDX(node, prop, idx)),  \
+		.pad = FIELD_GET(SIWX917_PINCTRL_PAD_MASK, DT_PROP_BY_IDX(node, prop, idx)),       \
 	},
 
-#define Z_PINCTRL_STATE_PIN_INIT(node_id, prop, idx)		\
-	SILABS_SIWX917_DT_PIN(DT_PROP_BY_IDX(node_id, prop, idx))
-
-#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)		\
-	{ DT_FOREACH_PROP_ELEM(node_id, prop, Z_PINCTRL_STATE_PIN_INIT) }
+#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)                                                   \
+	{DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop), DT_FOREACH_PROP_ELEM, pinmux,           \
+				Z_PINCTRL_STATE_PIN_INIT)}
 
 #endif
