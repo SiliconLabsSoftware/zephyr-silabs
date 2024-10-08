6d0f90217f2471da57b364e0b4cea53c49778291
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-10-08 15:21:08 +0200

    drivers: wifi: siwx917: Make offloading optional
    This new mode allows a better compatibility with Zephyr features.
    Support for offloading is still supported since it can provide better
    performances in some cases (power and memory consumption).
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/wifi/siwx917/siwx917_wifi_socket.h
+++ drivers/wifi/siwx917/siwx917_wifi_socket.h
@@ -8,12 +8,36 @@
 
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/offloaded_netdev.h>
+#include <assert.h>
 
 struct siwx917_dev;
+
+#ifdef CONFIG_WIFI_SIWX917_NET_STACK_OFFLOAD
 
 enum offloaded_net_if_types siwx917_get_type(void);
 void siwx917_on_join_ipv4(struct siwx917_dev *sidev);
 void siwx917_on_join_ipv6(struct siwx917_dev *sidev);
+void siwx917_sock_init(struct net_if *iface);
+
+#else /* CONFIG_WIFI_SIWX917_NET_STACK_OFFLOAD */
+
+enum offloaded_net_if_types siwx917_get_type(void)
+{
+	assert(0);
+}
+
+void siwx917_on_join_ipv4(struct siwx917_dev *sidev)
+{
+}
+
+void siwx917_on_join_ipv6(struct siwx917_dev *sidev)
+{
+}
+
 void siwx917_sock_init(struct net_if *iface)
+{
+}
+
+#endif /* CONFIG_WIFI_SIWX917_NET_STACK_OFFLOAD */
 
 #endif
