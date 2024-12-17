297ddc974f96e0e98e14f77ccc02fd07da0df233
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-12-17 16:41:01 +0100

    drivers: clock: siwx917: Add dumb clock driver
    This driver is mostly the initial seed for further implementation of a
    real clock driver.
    
    Currently, sinc ethe driver do not enable the clock sources, this driver
    still contains the series of #ifdef to enable the clocks.
    
    It also doesn't allow the user to choose the clock source for the
    various peripherals. The driver hardcodes some sane values.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- soc/silabs/silabs_siwx917/siwg917/soc.c
+++ soc/silabs/silabs_siwx917/siwg917/soc.c
@@ -15,48 +15,6 @@
 int silabs_siwx917_init(void)
 {
 	SystemInit();
-	SystemCoreClockUpdate();
-
-	/* Use SoC PLL at configured frequency as core clock */
-	sl_si91x_clock_manager_m4_set_core_clk(M4_SOCPLLCLK,
-					       CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
-
-	/* Use interface PLL at configured frequency as peripheral clock */
-	sl_si91x_clock_manager_set_pll_freq(INFT_PLL,
-					    CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
-					    PLL_REF_CLK_VAL_XTAL);
-
-	/* FIXME: do not hardcode UART instances */
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(ulpuart), okay)
-	RSI_ULPSS_UlpUartClkConfig(ULPCLK, ENABLE_STATIC_CLK, 0, ULP_UART_ULP_32MHZ_RC_CLK, 1);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay)
-	RSI_CLK_UsartClkConfig(M4CLK, ENABLE_STATIC_CLK, 0, USART1, 0, 1);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart2), okay)
-	RSI_CLK_UsartClkConfig(M4CLK, ENABLE_STATIC_CLK, 0, USART2, 0, 1);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c0), okay)
-	RSI_PS_M4ssPeriPowerUp(M4SS_PWRGATE_ULP_EFUSE_PERI);
-	RSI_CLK_I2CClkConfig(M4CLK, true, 0);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c1), okay)
-	RSI_PS_M4ssPeriPowerUp(M4SS_PWRGATE_ULP_EFUSE_PERI);
-	RSI_CLK_I2CClkConfig(M4CLK, true, 1);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(ulpi2c), okay)
-	RSI_PS_UlpssPeriPowerUp(ULPSS_PWRGATE_ULP_I2C);
-	RSI_ULPSS_PeripheralEnable(ULPCLK, ULP_I2C_CLK, ENABLE_STATIC_CLK);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(dma0), okay)
-	RSI_PS_M4ssPeriPowerUp(M4SS_PWRGATE_ULP_EFUSE_PERI);
-	RSI_CLK_PeripheralClkEnable(M4CLK, UDMA_CLK, ENABLE_STATIC_CLK);
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(ulpdma), okay)
-	RSI_PS_UlpssPeriPowerUp(ULPSS_PWRGATE_ULP_UDMA);
-	RSI_ULPSS_PeripheralEnable(ULPCLK, ULP_UDMA_CLK, ENABLE_STATIC_CLK);
-#endif
-
 	return 0;
 }
 SYS_INIT(silabs_siwx917_init, PRE_KERNEL_1, 0);
